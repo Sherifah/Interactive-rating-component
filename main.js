82ff1a5fa6b15917977194a0e84af5a77f191ad6
Sherifah <adelekebolaji0@gmail.com>
2022-10-21 04:11:32 +0100

    'feat'

---
+++ main.js
@@ -0,0 +1,31 @@
+const rateBtn = document.querySelectorAll('.rate');
+const btn = document.querySelector('button');
+const rateResult = document.querySelector('span');
+const rating = document.querySelector('.card-content-ratings');
+const thanks = document.querySelector('.card-content-thanks');
+
+//Represent initial rating
+let result = null;
+
+var clicked = false;
+function selectProcess(e) {
+    if (clicked) {
+        this.classList.toggle('selected');
+    }
+    this.classList.toggle('selected');
+    result = e.srcElement.textContent;
+}
+
+
+function onSubmit(e) {
+    thanks.hidden = false;
+    rating.hidden = true;
+    rateResult.textContent = result;
+}
+
+
+rateBtn.forEach((button) => { 
+    button.addEventListener('click', selectProcess)
+});
+
+btn.addEventListener('click', onSubmit);
